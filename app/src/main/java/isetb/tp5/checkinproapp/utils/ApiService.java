e0fbaee2fcddfa2f48dd51efe89e220a66517d04
sirin <garrouriserine@gmail.com>
2024-11-29 22:07:07 +0100

    add get and delete methods

--- app/src/main/java/isetb/tp5/checkinproapp/utils/ApiService.java
+++ app/src/main/java/isetb/tp5/checkinproapp/utils/ApiService.java
@@ -4,7 +4,10 @@ import isetb.tp5.checkinproapp.model.Employee;
 import isetb.tp5.checkinproapp.model.LoginRequest;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
     @POST("/Employee/register")
@@ -12,5 +15,13 @@ public interface ApiService {
 
     @POST("Employee/login") // Ajouter un endpoint de connexion sur votre backend si nécessaire
     Call<Employee> loginEmployee(@Body LoginRequest loginRequest);
-}
+
+    @GET("employees/{id}")
+        Call<Employee> getEmployeeById(@Path("id") Long id);
+
+    @DELETE("employees/{id}")
+        Call<Void> deleteEmployee(@Path("id") Long id);
+    }
+
+
 
